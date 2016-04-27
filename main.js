8a8cf875f965616197c8b32125e85022acb94292
Mitchdev <admin@mitchdev.net>
2016-04-27 20:45:03 +1200

    Create main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+$('.be-channel-page__nav__section be-channel-page__nav__section--bordered').append('<a onclick="farmingmode();" class="be-button be-channel-page__nav__menuitem md-ink-ripple">Farming Mode<div class="md-ripple-container"></div></a>');
+function farmingmode() {
+	
+}
