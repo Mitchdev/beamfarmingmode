f92cfbbf548cacb7d6d6fceef0e2d0508292f422
Mitchdev <admin@mitchdev.net>
2016-04-27 21:06:39 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 $('.a-d-3').append('<a onclick="farmingmode();" class="be-button be-channel-page__nav__menuitem md-ink-ripple">Farming Mode<div class="md-ripple-container"></div></a>');
 function farmingmode() {
 	$('.be-navbar__dashboardbutton .be-button').click();
-	$('head').append('<link rel="stylesheet" href="https://raw.githubusercontent.com/Mitchdev/beamfarmingmode/master/main.css">')
+	$('head').append('<link rel="stylesheet" href="https://raw.githubusercontent.com/Mitchdev/beamfarmingmode/master/main.css">');
+	$('body').append('<a onclick="exitfarmingmode();" class="exitfarmingmode be-button be-channel-page__nav__menuitem md-ink-ripple">Exit Farming Mode<div class="md-ripple-container"></div></a>');
 }
-$('.be-navbar__logo').on('click', function() {
-	$('.farmingmodecss').remove();	
-});
+function exitfarmingmode() {
+	parent.history.back();
+	$('.exitfarmingmode').remove();
+	$('.farmingmodecss').remove();
+}
