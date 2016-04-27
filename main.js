ab401750107f4fe1a9a4059e71891924372e361b
Mitchdev <admin@mitchdev.net>
2016-04-27 22:07:35 +1200

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ console.log('waiting for beam to load.');
 setTimeout(function() {
 	console.log('farming mode is now running.');
 	$('.a-d-3').append('<a style="display: none;" class="farmingmode be-button be-channel-page__nav__menuitem md-ink-ripple">Farming Mode<div class="md-ripple-container"></div></a>');
-	$('.text-center').append('<br><br><br><a style="display: none;font-size: 25px;" onclick="exitfarmingmode();">Exit Farming Mode</a>');
 	$('.farmingmode').fadeToggle('slow');
 	$('.farmingmode').on('click', function() {
 		$('.be-navbar__dashboardbutton .be-button').click();
 		$('head').append('<link rel="stylesheet" href="https://mitchdev.github.io/beamfarmingmode/main.css">');
-		$('.exitfarmingmode').fadeToggle('slow');
+		$('.text-center').append('<br><br><br><a style="display: none;font-size: 25px;" onclick="exitfarmingmode();">Exit Farming Mode</a>');
 	});
 	function exitfarmingmode() {
 		parent.history.back();
