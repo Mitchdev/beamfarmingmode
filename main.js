0bb7fc50d166b4d4b143541cb44ca247097dfa98
Mitchdev <admin@mitchdev.net>
2016-04-27 21:34:30 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-$('.a-d-3').append('<a onclick="farmingmode();" class="be-button be-channel-page__nav__menuitem md-ink-ripple">Farming Mode<div class="md-ripple-container"></div></a>');
-function farmingmode() {
-	$('.be-navbar__dashboardbutton .be-button').click();
-	$('head').append('<link rel="stylesheet" href="https://rawgit.com/Mitchdev/beamfarmingmode/master/main.css">');
-	$('body').append('<a onclick="exitfarmingmode();" class="exitfarmingmode be-button be-channel-page__nav__menuitem md-ink-ripple">Exit Farming Mode<div class="md-ripple-container"></div></a>');
-}
-function exitfarmingmode() {
-	parent.history.back();
-	$('.exitfarmingmode').remove();
-	$('.farmingmodecss').remove();
-}
+$(function() {
+	console.log('farming mode running');
+	$('.a-d-3').append('<a onclick="farmingmode();" class="be-button be-channel-page__nav__menuitem md-ink-ripple">Farming Mode<div class="md-ripple-container"></div></a>');
+	function farmingmode() {
+		$('.be-navbar__dashboardbutton .be-button').click();
+		$('head').append('<link rel="stylesheet" href="https://rawgit.com/Mitchdev/beamfarmingmode/master/main.css">');
+		$('body').append('<a onclick="exitfarmingmode();" class="exitfarmingmode be-button be-channel-page__nav__menuitem md-ink-ripple">Exit Farming Mode<div class="md-ripple-container"></div></a>');
+	}
+	function exitfarmingmode() {
+		parent.history.back();
+		$('.exitfarmingmode').remove();
+		$('.farmingmodecss').remove();
+	}
+});
