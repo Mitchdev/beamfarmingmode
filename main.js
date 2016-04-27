f5eee6483ca437afd456cf9aa402dd072df91ed6
Mitchdev <admin@mitchdev.net>
2016-04-27 20:49:01 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 $('.be-channel-page__nav__section be-channel-page__nav__section--bordered').append('<a onclick="farmingmode();" class="be-button be-channel-page__nav__menuitem md-ink-ripple">Farming Mode<div class="md-ripple-container"></div></a>');
 function farmingmode() {
-	
+	$('head').append('<link rel="stylesheet" href="https://raw.githubusercontent.com/Mitchdev/beamfarmingmode/master/main.css">')
 }
+$('.be-navbar__logo').on('click', function() {
+	$('.farmingmodecss').remove();	
+});
