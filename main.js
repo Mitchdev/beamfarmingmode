cb6d75dbb966f8834aa95c8ee37c84acb03aae13
Mitchdev <admin@mitchdev.net>
2016-04-27 21:46:07 +1200

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ setTimeout(function() {
 	$('.a-d-3').append('<a onclick="farmingmode();" class="be-button be-channel-page__nav__menuitem md-ink-ripple">Farming Mode<div class="md-ripple-container"></div></a>');
 	function farmingmode() {
 		$('.be-navbar__dashboardbutton .be-button').click();
-		$('head').append('<link rel="stylesheet" href="https://rawgit.com/Mitchdev/beamfarmingmode/master/main.css">');
+		$('head').append('<link rel="stylesheet" href="https://mitchdev.github.io/beamfarmingmode/main.css">');
 		$('.text-center').append('<span onclick="exitfarmingmode();">Exit Farming Mode</span>');
 	}
 	function exitfarmingmode() {
