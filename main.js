88e17b8b8bc240ac35a5d5014af2409282e182b0
Mitchdev <admin@mitchdev.net>
2016-04-27 21:55:39 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 console.log('waiting for beam to load.');
 setTimeout(function() {
 	console.log('farming mode is now running.');
-	$('.a-d-3').append('<a class="farmingmode be-button be-channel-page__nav__menuitem md-ink-ripple">Farming Mode<div class="md-ripple-container"></div></a>');
+	$('.a-d-3').append('<a style="display: none;" class="farmingmode be-button be-channel-page__nav__menuitem md-ink-ripple">Farming Mode<div class="md-ripple-container"></div></a>');
+	$('.farmingmode').fadeToggle('slow');
 	$('.farmingmode').on('click', function() {
 		$('.be-navbar__dashboardbutton .be-button').click();
 		$('head').append('<link rel="stylesheet" href="https://mitchdev.github.io/beamfarmingmode/main.css">');
