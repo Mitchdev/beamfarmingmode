5cdb815e205e801f6adb642dc724e6673a3d998c
Mitchdev <admin@mitchdev.net>
2016-05-06 17:57:36 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.log('waiting for beam to load.');
 setTimeout(function() {
 	console.log('farming mode is now running.');
-	$('.be-channel-page__btns').append('<div class="be-u-m--l1 ng-scope" onclick="farmingmode();"><button class="md-small md-no-spacing md-button md-ink-ripple"><span>Farming Mode</span></button></div>');
+	$('.be-channel-page__btns').append('<div class="be-u-m--l1 ng-scope"><button class="md-small md-no-spacing md-button md-ink-ripple"><span onclick="farmingmode();">Farming Mode</span></button></div>');
 	setTimeout(function() {
 	function farmingmode() {
 		$('.be-navbar__dashboardbutton .be-button').click();
