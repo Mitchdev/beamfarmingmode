4d71cb99db0eb8b8e0989b717e510f4a84fd2ace
Mitchdev <admin@mitchdev.net>
2016-05-06 19:04:06 +1200

    Update main.js

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ setTimeout(function() {
 	console.log('farming mode is now running.');
 	$('.be-channel-page__btns').append('<div class="farmingmode be-u-m--l1 ng-scope"><button class="md-small md-no-spacing md-button md-ink-ripple"><span>Farming Mode</span></button></div>');
 	$('.farmingmode').on('click', function() {
-		$('.be-navbar__dashboardbutton .be-button').click();
-		$('head').append('<link rel="stylesheet" href="https://rawgit.com/Mitchdev/beamfarmingmode/master/main.css">');
-		$('.text-center').append('<span onclick="exitfarmingmode();">Exit Farming Mode</span>');
+		$('.be-hub__activator button').click();
+	//	$('.be-hub__section__item--menu').click();
+	//	$('head').append('<link rel="stylesheet" href="https://rawgit.com/Mitchdev/beamfarmingmode/master/main.css">');
+	//	$('.text-center').append('<span onclick="exitfarmingmode();">Exit Farming Mode</span>');
 	});
-	function exitfarmingmode() {
-		parent.history.back();
-		$('.exitfarmingmode').remove();
-		$('.farmingmodecss').remove();
-	}
+	//function exitfarmingmode() {
+	//	parent.history.back();
+	//	$('.exitfarmingmode').remove();
+	//	$('.farmingmodecss').remove();
+	//}
 }, 6000);
