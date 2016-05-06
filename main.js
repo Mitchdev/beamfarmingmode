2fdb052a71d13f3dff667c69dae59b9efa9926a9
Mitchdev <admin@mitchdev.net>
2016-05-06 21:19:52 +1200

    Update main.js

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ console.log('waiting for beam to load.');
 setTimeout(function() {
 	console.log('farming mode is now running.');
 	$('.be-channel-page__btns').append('<div class="farmingmode be-u-m--l1 ng-scope"><button class="md-small md-no-spacing md-button md-ink-ripple"><span>Farming Mode</span></button></div>');
-	$('.a-d-3').prepend('<div class="exitbfm" style="display:none;"><button class="md-raised btn-fixed-size-medium md-button md-ink-ripple" type="button"><span class="ng-scope">Exit Farming Mode</span></button></div>');
 	$('.farmingmode').on('click', function() {
 		$('.be-hub').addClass('be-hub--active');
 		$('.be-hub__menu').find('.be-u-m--t1').children().first().find('.md-ink-ripple').click();
 		//$('head').append('<link rel="stylesheet" href="https://rawgit.com/Mitchdev/beamfarmingmode/master/main.css">');
-		$('.a-d-3').prepend('<div class="exitbfm" onclick="exitfarmingmode()"><button class="md-raised btn-fixed-size-medium md-button md-ink-ripple" type="button"><span class="ng-scope">Exit Farming Mode</span></button></div>');
+		$('.a-d-3').prepend('<div class="exitbfm"><button class="md-raised btn-fixed-size-medium md-button md-ink-ripple" type="button"><span class="ng-scope">Exit Farming Mode</span></button></div>');
 	});
 	$('.exitbfm').on('click', function() {
 		parent.history.back();
+		$('.exitfm').remove();
 		//$('.farmingmodecss').remove();
 	});
 }, 6000);
