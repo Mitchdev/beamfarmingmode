3af91310dd49254fa3be0d347a24495fb01b79ea
Mitchdev <admin@mitchdev.net>
2016-05-06 17:23:21 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
 console.log('waiting for beam to load.');
 setTimeout(function() {
-	//var username;
 	console.log('farming mode is now running.');
-	//username = $('.be-user-name').text;
-	$('.a-d-3').append('<a style="display: none;" class="farmingmode be-button be-channel-page__nav__menuitem md-ink-ripple">Farming Mode<div class="md-ripple-container"></div></a>');
-	$('.farmingmode').fadeToggle('slow');
-	$('.farmingmode').on('click', function() {
+	$('.be-channel-page__nav__section be-channel-page__btns').append('<div class="ng-scope" onclick="farmingmode();"><b id="$vm.channel.id" class="ng-isolate-scope"><div ng-mouseenter="$vm.hover = true" ng-mouseleave="$vm.hover = false"><span class="ng-scope"><button class="md-raised md-small md-primary md-button ng-scope md-ink-ripple" type="button"><div style="width: 75px" class="ng-scope"><span class="ng-scope"><span class="icon-heart-full"></span> <span class="hide-md">Farming Mode</span></span></div><div class="md-ripple-container"></div></button></span></div></b></div>');
+	function farmingmode() {
 		$('.be-navbar__dashboardbutton .be-button').click();
-		$('head').append('<link class="farmingmodecss" rel="stylesheet" href="https://mitchdev.github.io/beamfarmingmode/main.css">');
-		$('.text-center').append('<br><br><br><a style="font-size: 25px;" class="exitfarmingmode">Exit Farming Mode</a>');
-		$('.exitfarmingmode').on('click', function() {
-			//location.href = 'https://beam.pro/'+username;
-			parent.history.back();
-			$('.exitfarmingmode').remove();
-			$('.farmingmodecss').remove();
-		});
-	});
-}, 4000);
+		$('head').append('<link rel="stylesheet" href="https://rawgit.com/Mitchdev/beamfarmingmode/master/main.css">');
+		$('.text-center').append('<span onclick="exitfarmingmode();">Exit Farming Mode</span>');
+	}
+	function exitfarmingmode() {
+		parent.history.back();
+		$('.exitfarmingmode').remove();
+		$('.farmingmodecss').remove();
+	}
+}, 6000);
