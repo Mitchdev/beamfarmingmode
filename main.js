cff82468e279df3d05f8357fe870e03a5c274af7
Mitchdev <admin@mitchdev.net>
2016-05-06 17:42:50 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.log('waiting for beam to load.');
 setTimeout(function() {
 	console.log('farming mode is now running.');
-	$('.be-channel-page__btns').append('<div class="be-u-m--l1 ng-scope"><button class="md-small md-no-spacing md-button md-ink-ripple" onclick="farmingmode();"><span>Farming Mode</span></button></div>');
+	$('.be-channel-page__btns').append('<div class="be-u-m--l1 ng-scope" onclick="farmingmode();"><button class="md-small md-no-spacing md-button md-ink-ripple"><span>Farming Mode</span></button></div>');
 	function farmingmode() {
 		$('.be-navbar__dashboardbutton .be-button').click();
 		$('head').append('<link rel="stylesheet" href="https://rawgit.com/Mitchdev/beamfarmingmode/master/main.css">');
