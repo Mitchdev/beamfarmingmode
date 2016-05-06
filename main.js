04868ece717a8e1169575c6a1e2b3df626f26d83
Mitchdev <admin@mitchdev.net>
2016-05-06 19:20:21 +1200

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ setTimeout(function() {
 		$('.be-channel-page__btns').append('<div class="farmingmode be-u-m--l1 ng-scope"><button class="md-small md-no-spacing md-button md-ink-ripple"><span>Farming Mode</span></button></div>');
 	}
 	$('.farmingmode').on('click', function() {
-		$('.be-hub__activator .md-ink-ripple').click();
+		$('.be-hub').addClass('be-hub--open');
 	//	$('head').append('<link rel="stylesheet" href="https://rawgit.com/Mitchdev/beamfarmingmode/master/main.css">');
 	//	$('.text-center').append('<span onclick="exitfarmingmode();">Exit Farming Mode</span>');
 	});
