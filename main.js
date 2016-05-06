eaed87bdd889a8714821b88041785efd4bb81e21
Mitchdev <admin@mitchdev.net>
2016-05-06 17:59:10 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
 console.log('waiting for beam to load.');
 setTimeout(function() {
 	console.log('farming mode is now running.');
-	$('.be-channel-page__btns').append('<div class="be-u-m--l1 ng-scope"><button class="md-small md-no-spacing md-button md-ink-ripple"><span onclick="farmingmode();">Farming Mode</span></button></div>');
-	setTimeout(function() {
-	function farmingmode() {
+	$('.be-channel-page__btns').append('<div class="farmingmode be-u-m--l1 ng-scope"><button class="md-small md-no-spacing md-button md-ink-ripple"><span>Farming Mode</span></button></div>');
+	$('.farmingmode').on('click', function() {
 		$('.be-navbar__dashboardbutton .be-button').click();
 		$('head').append('<link rel="stylesheet" href="https://rawgit.com/Mitchdev/beamfarmingmode/master/main.css">');
 		$('.text-center').append('<span onclick="exitfarmingmode();">Exit Farming Mode</span>');
-	}
+	});
 	function exitfarmingmode() {
 		parent.history.back();
 		$('.exitfarmingmode').remove();
 		$('.farmingmodecss').remove();
 	}
-	}, 2000);
 }, 6000);
