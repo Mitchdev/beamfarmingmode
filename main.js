f4e7dea072102b2154a9b937c1ee439e7f93947f
Mitchdev <admin@mitchdev.net>
2016-05-08 12:31:54 +1200

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ setTimeout(function() {
 		$('.be-hub').addClass('be-hub--active');
 		$('.be-hub__menu').find('.be-u-m--t1').children().first().find('.md-ink-ripple').click();
 		$('head').append('<link class="farmingmodecss" rel="stylesheet" href="https://mitchdev.github.io/beamfarmingmode/main.css">');
-		$('.a-d-3').prepend('<div class="exitbfm"><button style="background-color: rgba(255,255,255,0.3)" class="btn-fixed-size-medium md-button md-ink-ripple" type="button"><span class="ng-scope">Exit Farming Mode</span></button></div>');
+		$('.dash-content').find('.a-d-3').prepend('<div class="exitbfm"><button style="background-color: rgba(255,255,255,0.3)" class="btn-fixed-size-medium md-button md-ink-ripple" type="button"><span class="ng-scope">Exit Farming Mode</span></button></div>');
 	});
 	$('body').on('click', '.t-d-4 .layout-wrap b-browse-card md-card', function() {
 		setTimeout(function() {
