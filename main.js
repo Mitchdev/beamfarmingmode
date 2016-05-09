bb6f5017ffc3104c2242c730943fc40f36a747f5
Mitchdev <admin@mitchdev.net>
2016-05-09 17:13:47 +1200

    Update main.js

--- main.js
+++ main.js
@@ -16,13 +16,13 @@ setTimeout(function() {
 		sparks = true;
 	});
 	setInterval(function() {
-		var second = $('.be-hub__section--points .be-hub__section__item').find('.flex-2 small').text().slice(-7, -1);
+		var second = $('.be-hub__section--points .be-hub__section__item').find('.flex-2 small').text().slice(-6, -1);
 		var progress = $('.be-hub__section--points .be-hub__section__item').find('.flex-2 .md-white').attr('aria-valuenow');
 		var amount = $('.be-hub__section--points .be-hub__section__item').find('div:not(.flex-2) span.ng-binding').text();
 		console.log(amount);
 		if (sparks) {
 			$('.bfm-sparks div').css('width', progress+'%');
-			$('.bfm-sparks-small').html('4 <span class="icon-spark"></span>'+second+'s');
+			$('.bfm-sparks-small').html('4 <span class="icon-spark"></span> '+second+'s');
 			$('.be-user-sparks.be-user-sparks__progress.a.fadeInLeft.a-d-1 b').text(amount);
 		}
 	}, 1000);
